5d4542d21bf9b2df6ff6cb90472efc1800842cbe
Domingos Rodrigues <domingos.rodrigues@inventvision.com.br>
2023-01-19 18:49:50 -0300

    feat: add ubuntu bionic version

---
+++ docker/third_party/get_debian_pkgs.sh
@@ -0,0 +1,3 @@
+wget https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb
+wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+wget https://github.com/charmbracelet/glow/releases/download/v1.4.1/glow_1.4.1_linux_amd64.deb
